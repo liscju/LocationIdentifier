8f36840039c1c76af9137bebcb96d0256d0aa1a2
liscju <piotr.listkiewicz@gmail.com>
2016-02-01 20:07:26 +0100

    Add recognising qrcodes, printing result as toast and speeching it

--- app/src/main/java/pl/edu/agh/locationidentifier/MainActivity.java
+++ app/src/main/java/pl/edu/agh/locationidentifier/MainActivity.java
@@ -1,15 +1,23 @@
 package pl.edu.agh.locationidentifier;
 
+import android.graphics.PointF;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
+    public static final int TIME_BETWEEN_NOTIFICATION = 5000;
+    long lastTimeUserWasNotifiedAboutBarcode = 0;
+    TextToSpeech textToSpeech;
+    private QRCodeReaderView mydecoderview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        textToSpeech=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
     }
 
     @Override
@@ -34,5 +54,36 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        long currentTimeMillis = System.currentTimeMillis();
+        if (currentTimeMillis - lastTimeUserWasNotifiedAboutBarcode >= TIME_BETWEEN_NOTIFICATION) {
+            lastTimeUserWasNotifiedAboutBarcode = currentTimeMillis;
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public void cameraNotFound() {
+        Toast.makeText(this, "Error: Camera not found", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.getCameraManager().startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.getCameraManager().stopPreview();
     }
 }
